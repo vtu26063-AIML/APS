f2e8cd76290ae68314f2dc3be2a9bc662bc0bc37
vtu26063-AIML <vtu26063@veltech.edu.in>
2026-04-21 12:49:55 +0530

    Add files via upload

---
+++ week6/FindKPairswithSmallestSums.java
@@ -0,0 +1,28 @@
+import java.util.*;
+
+class Solution {
+    public List<List<Integer>> kSmallestPairs(int[] nums1, int[] nums2, int k) {
+        List<List<Integer>> res = new ArrayList<>();
+        if (nums1.length == 0 || nums2.length == 0) return res;
+
+        PriorityQueue<int[]> pq = new PriorityQueue<>(
+            (a, b) -> (a[0] + a[1]) - (b[0] + b[1])
+        );
+
+        for (int i = 0; i < nums1.length && i < k; i++) {
+            pq.add(new int[]{nums1[i], nums2[0], 0});
+        }
+
+        while (k > 0 && !pq.isEmpty()) {
+            int[] cur = pq.poll();
+            res.add(Arrays.asList(cur[0], cur[1]));
+
+            if (cur[2] + 1 < nums2.length) {
+                pq.add(new int[]{cur[0], nums2[cur[2] + 1], cur[2] + 1});
+            }
+            k--;
+        }
+
+        return res;
+    }
+}
---
+++ week6/TopKFrequentElements.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+class Solution {
+    public int[] topKFrequent(int[] nums, int k) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int n : nums)
+            map.put(n, map.getOrDefault(n, 0) + 1);
+
+        PriorityQueue<Integer> pq = new PriorityQueue<>(
+            (a, b) -> map.get(a) - map.get(b)
+        );
+
+        for (int key : map.keySet()) {
+            pq.add(key);
+            if (pq.size() > k) pq.poll();
+        }
+
+        int[] res = new int[k];
+        for (int i = k - 1; i >= 0; i--)
+            res[i] = pq.poll();
+
+        return res;
+    }
+}
